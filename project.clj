c0fa7a8085fb07ec2c6cfbb6b3a57ca8a7e76032
Sean Corfield <sean@corfield.org>
2013-07-17 21:02:26 -0700

    Basic skeleton.

---
+++ project.clj
@@ -0,0 +1,8 @@
+(defproject spd1 "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :plugins [[lein-expectations "0.0.8"]]
+  :profiles {:dev {:dependencies [[expectations "1.4.49"]]}})
