02243ca3b9c36688c6771a57dbccd433c54c8403
Sean Corfield <sean@corfield.org>
2013-07-17 21:02:41 -0700

    Add sum-n-solution as first example.

---
+++ src/spd1/sum_n_solution.clj
@@ -0,0 +1,12 @@
+(ns spd1.sum-n-solution)
+
+#_(defn sum-n-odds
+  "Natural -> Natural
+   Produce the sum of the first n odd numbers."
+  [n] 0) ; stub
+
+(defn sum-n-odds
+  "Natural -> Natural
+   Produce the sum of the first n odd numbers."
+  [n]
+  (reduce + 0 (filter odd? (range (* 2 n)))))
