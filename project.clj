109b5b8e18dc78cd2b68217bd02113f1891d2d0f
Sean Corfield <sean@worldsingles.com>
2013-07-25 22:53:31 -0700

    Simplify dependencies etc and update description / url to provide useful information.

--- project.clj
+++ project.clj
@@ -1,9 +1,7 @@
 (defproject spd1 "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Clojure versions of exercises from Introduction to Systematic Program Design Part 1 on Coursera"
+  :url "https://github.com/seancorfield/spd1"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :plugins [[lein-autoexpect "0.2.5"]
-            [lein-expectations "0.0.8"]]
-  :profiles {:dev {:dependencies [[expectations "1.4.49"]]}})
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [expectations "1.4.49"]])
