1c5f3ab0eb61674b5e289c3f7484dcd2fa94738b
Sean Corfield <sean@worldsingles.com>
2013-07-26 00:26:34 -0700

    Flesh out to more closely match the BSL version

--- src/spd1/sum_n.clj
+++ src/spd1/sum_n.clj
@@ -4,6 +4,13 @@
 
 (clear-tests [*ns*])
 
+(comment
+  "PROBLEM:
+
+  Complete the design of the following function, by writing out
+  the final function definition. Use at least one built in abstract
+  function.")
+
 ;; declare function ahead of examples
 (declare sum-n-odds)
 
@@ -12,16 +19,38 @@
 (expect (+ 0 1) (sum-n-odds 1))
 (expect (+ 0 1 3 5) (sum-n-odds 3))
 
+(comment
+  "HINT: The first n odd numbers are contained in the first 2*n naturals.
+  For example (list 0 1 2 3) contains the first 4 naturals and the first
+  2 odd numbers. Also remember that DrRacket has a build in predicate
+  function called odd?")
+
 #_(defn sum-n-odds
   "Natural -> Natural
    Produce the sum of the first n odd numbers."
   [n] 0) ; stub
 
+;; SOLUTION CHOICE ONE
+
+;; reduce is Clojure's folder; (range n) is Clojure's (build-list n identity)
 (defn sum-n-odds
   "Natural -> Natural
    Produce the sum of the first n odd numbers."
   [n]
   (reduce + 0 (filter odd? (range (* 2 n)))))
+
+;; build-list if you wanted it
+(defn build-list [n f] (map f (range n)))
+
+;; SOLUTION CHOICE TWO
+
+;; letfn is how Clojure declares local functions
+(defn sum-n-odds
+  "Natural -> Natural
+   Produce the sum of the first n odd numbers."
+  [n]
+  (letfn [(multiply [i] (+ (* 2 i) 1))]
+    (reduce + 0 (build-list n multiply))))
 
 ;; automatically run tests
 (run-tests [*ns*])
